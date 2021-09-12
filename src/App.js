eddfa0ab3bfc0984ca727f5e4008de3ce9fd3d78
delongamateo <delongamateo@gmail.com>
2021-09-12 15:51:19 +0200

    added router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import React, { Component } from "react"
-import Layout from "./components/Layout/Layout";
 import "./App.scss";
+import MiniCart from "./components/MiniCart/MiniCart";
+import ProductPage from "./components/ProductPage/ProductPage";
+import Cart from "./components/Cart/Cart";
+import Category from "./components/Category/Category";
+import Nav from "./components/Nav/Nav";
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      category: "Women",
+      miniCart: false,
+    };
+  }
+
+  updateCategory = (props) => {
+    this.setState({ category: props });
+  };
+
+  showMiniCart = () => {
+    if (this.state.miniCart === false) {
+      this.setState({ miniCart: true });
+    } else {
+      this.setState({ miniCart: false });
+    }
+  };
+  
   render() {
     return (
       <div className="app">
-        <Layout />
+        <div className="container">
+          <Nav
+            updateCategory={this.updateCategory}
+            showMiniCart={this.showMiniCart}
+            category={this.state.category}
+          />
+          {this.state.miniCart && <MiniCart />}
+          
+        </div>
       </div>
     );
   }
